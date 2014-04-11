5899cdf650ae14f200b60dfde13b4f22ac0a757f
Benjamin LECHIARA <blechiar@e2r12p6.42.fr>
2014-04-11 10:46:54 +0200

    loup

--- Class/Ship.class.php
+++ Class/Ship.class.php
@@ -11,8 +11,24 @@ class Ship
 	{
 		$this->width = $width;
 		$this->height = $height;
-		$this->direction = 0;
+		$this->direction = 3;
 		$this->position = array('x' => $x, 'y' => $y);
+	}
+
+	public function 	moveUp(){
+		$this->position['x'] = $this->position['x'] + 1;
+	}
+
+	public function 	rotateLeft(){
+		$this->direction = $this->direction + 1;
+		if ($this->direction > 3)
+			$this->direction = 0;
+	}
+
+	public function 	rotateRight(){
+		$this->direction = $this->direction - 1;
+		if ($this->direction < 0)
+			$this->direction = 3;
 	}
 }
 
